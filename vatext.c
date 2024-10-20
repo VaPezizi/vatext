2bf82edfb0d0b98e489d0a830f82a1a3f15503d9
Samuli Pihlaja <samuli.pihlaja.SP@gmail.com>
2024-10-20 17:29:23 +0300

    Added fallback method for getting window size, if ioctl fails

--- vatext.c
+++ vatext.c
@@ -62,11 +62,30 @@ char readKey(){
 	return c;
 }
 
+int getCursorPosition(int *rows, int * columns){
+	char buffer[32];
+	unsigned int i = 0;
+	
+	if(write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+	while (i < sizeof(buffer) - 1){
+		if(read(STDIN_FILENO, &buffer[i], 1) != 1)break;
+		if(buffer[i] == 'R')break;
+		i++;
+	}
+	buffer[i] = '\0';
+
+	if (buffer[0] != '\x1b' || buffer[1] != '[') return -1;
+	if (sscanf(&buffer[2], "%d;%d", rows, columns) != 2) return -1;
+	return 0;	
+}
+
 int getWindowSize(int *rows, int * columns){
 	struct winsize ws;
 
 	if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws)==-1 || ws.ws_col == 0){	//TIOCGWINSZ is just a request wich gives the current window size (Even tough it looks scary)
-		return -1;							
+		if(write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+		return getCursorPosition(rows, columns);			//Using the fallback method 
 	}else{
 		*columns = ws.ws_col;
 		*rows = ws.ws_row;
